6e51016047f02fafca8e20cd0c422162f922aa0a
Ismo <43433789+IzmoMac@users.noreply.github.com>
2025-05-24 15:47:20 +0200

    Update AppLayout.tsx

--- basemicrosaasapp.client/src/components/AppLayout.tsx
+++ basemicrosaasapp.client/src/components/AppLayout.tsx
@@ -8,7 +8,7 @@ const AppLayout = () => {
         <div className="flex">
 
             <ResponsiveNav />
-            <main className="flex-1 ml-0 sm:ml-64 pb-16 sm:pb-0 sm:overflow-y-auto">
+            <main className="flex-1 ml-0 sm:ml-64 pb-16 sm:pb-0">
                 <Outlet />
             </main>
         </div>
